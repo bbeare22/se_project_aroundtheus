f18e6ce63626306be108854ca69e8b0ff8daa55c
BrettBeare <beare.logan@yahoo.com>
2025-05-22 20:05:38 -0600

    Project 9 First commit, adding API and PopupWithConfirm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import "./index.css";
 import {
-  initialCards,
   selectors,
   validationSettings,
   getDOMElements,
@@ -9,12 +8,25 @@ import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
-import UserInfo from "../components/UserInfo.js";
 import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithConfirm from "../components/PopupWithConfirm.js";
+import UserInfo from "../components/UserInfo.js";
+import Api from "../components/Api.js";
 
 let editFormValidator;
 let addFormValidator;
+let avatarFormValidator;
 let dom;
+let cardSection;
+let userId;
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "806a2baa-081d-4321-b473-e51300bcf632",
+    "Content-Type": "application/json",
+  },
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   dom = getDOMElements();
@@ -22,8 +34,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const userInfo = new UserInfo({
     nameSelector: "#profile-title",
     jobSelector: "#profile-description",
+    avatarSelector: ".profile__image",
   });
 
+  // Validators
   editFormValidator = new FormValidator(
     validationSettings,
     dom.profileEditForm
@@ -33,54 +47,117 @@ document.addEventListener("DOMContentLoaded", () => {
   addFormValidator = new FormValidator(validationSettings, dom.cardAddForm);
   addFormValidator.enableValidation();
 
-  const cardSection = new Section(
-    {
-      items: initialCards,
-      renderer: renderCard,
-    },
+  avatarFormValidator = new FormValidator(
+    validationSettings,
+    dom.avatarEditForm
+  );
+  avatarFormValidator.enableValidation();
+
+  // Confirm Delete Modal
+  const confirmDeletePopup = new PopupWithConfirm({
+    popupSelector: "#confirm-delete-modal",
+  });
+  confirmDeletePopup.setEventListeners();
+
+  // Card Section
+  cardSection = new Section(
+    { items: [], renderer: renderCard },
     selectors.cardSection
   );
-  cardSection.renderItems();
 
-  function renderCard(item) {
-    const card = new Card(item, selectors.cardTemplate, handleImageClick);
-    const cardEl = card.getView();
-    cardSection.addItem(cardEl);
-  }
-
+  // Add Card Popup
   const cardAddPopupWithForm = new PopupWithForm(
     "#add-card-modal",
     (values) => {
-      return new Promise((resolve) => {
-        renderCard({
-          name: values.title,
-          link: values.url,
+      return api
+        .addCard({ name: values.title, link: values.url })
+        .then((newCard) => {
+          renderCard(newCard);
+          cardAddPopupWithForm.close(); // ← close on success
+          addFormValidator.disableSubmitButton();
+        })
+        .catch((err) => {
+          console.error("Error adding card:", err);
         });
-        addFormValidator.disableSubmitButton();
-        resolve(); // Indicate that submission is done
-      });
     }
   );
   cardAddPopupWithForm.setEventListeners();
 
-  const profileEditSubmitPopupWithForm = new PopupWithForm(
+  // Edit Profile Popup
+  const profileEditPopup = new PopupWithForm(
     "#profile-edit-modal",
     (values) => {
-      return new Promise((resolve) => {
-        userInfo.setUserInfo(values);
-        resolve();
-      });
+      return api
+        .updateUserInfo({ name: values.name, about: values.about })
+        .then((updatedUser) => {
+          userInfo.setUserInfo(updatedUser);
+        })
+        .catch((err) => {
+          console.error("Error updating profile:", err);
+        });
     }
   );
-  profileEditSubmitPopupWithForm.setEventListeners();
+  profileEditPopup.setEventListeners();
 
+  // Edit Avatar Popup
+  const avatarEditPopup = new PopupWithForm("#avatar-edit-modal", (values) => {
+    return api
+      .updateAvatar(values.avatar)
+      .then((res) => {
+        userInfo.setAvatar(res.avatar);
+      })
+      .catch((err) => {
+        console.error("Error updating avatar:", err);
+      });
+  });
+  avatarEditPopup.setEventListeners();
+
+  // Image Preview Popup
   const imagePopup = new PopupWithImage("#popup-preview-modal");
   imagePopup.setEventListeners();
 
+  // Load initial data
+  api
+    .getAppData()
+    .then(([userData, cards]) => {
+      userId = userData._id;
+      userInfo.setUserInfo(userData);
+      cardSection.renderItems(cards);
+    })
+    .catch((err) => console.error("Initialization error:", err));
+
+  // Renders a single card, wiring up delete confirmation
+  function renderCard(item) {
+    const card = new Card(
+      item,
+      selectors.cardTemplate,
+      handleImageClick,
+      (cardInstance, cardId) => {
+        confirmDeletePopup.setSubmitAction(() => {
+          confirmDeletePopup.renderSaving(true);
+          api
+            .deleteCard(cardId)
+            .then(() => {
+              cardInstance.removeCard();
+              confirmDeletePopup.close();
+            })
+            .catch((err) => console.error("Error deleting card:", err))
+            .finally(() => {
+              confirmDeletePopup.renderSaving(false);
+            });
+        });
+        confirmDeletePopup.open();
+      }
+    );
+
+    cardSection.addItem(card.getView());
+  }
+
+  // Button listeners
   dom.profileEditButton.addEventListener("click", () => {
     editFormValidator.resetValidation();
-    profileEditSubmitPopupWithForm.setInputValues(userInfo.getUserInfo());
-    profileEditSubmitPopupWithForm.open();
+    profileEditPopup.setInputValues(userInfo.getUserInfo());
+    profileEditPopup.open();
   });
 
   dom.cardAddButton.addEventListener("click", () => {
@@ -88,6 +165,12 @@ document.addEventListener("DOMContentLoaded", () => {
     cardAddPopupWithForm.open();
   });
 
+  dom.avatarEditButton.addEventListener("click", () => {
+    avatarFormValidator.resetValidation();
+    avatarEditPopup.open();
+  });
+
+  // Image click handler
   function handleImageClick(cardData) {
     imagePopup.open(cardData);
   }
